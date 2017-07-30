84d4072422d3b049756759cf93ae64b3cfeb703c
Fabio A. Camichel <fabiocamichel@gmail.com>
2017-07-30 11:28:07 +0200

    test: renders 3 shelves for books
    Added test to check if there are 3 shelves for books on the main page.
    Using shallow and mount from enzyme.

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import { shallow, mount } from 'enzyme';
+import App from './App';
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
+/**
+ This course is not designed to teach Test Driven Development.
+ Feel free to use this file to test your application, but it
  is not required.
 **/
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+describe('App component', () => {
+
+    it('renders without crashing', () => {
+        const wrapper = shallow(<App />);
+    })
+
+    it('renders 3 shelves for books', () => {
+        const mountWrapper = mount(<BrowserRouter><App /></BrowserRouter>);
+        const currently = <h2 className="bookshelf-title">Currently Reading</h2>;
+        const wantToRead = <h2 className="bookshelf-title">Want to Read</h2>
+        const read = <h2 className="bookshelf-title">Read</h2>
+
+        expect(mountWrapper.contains(currently)).toEqual(true);
+        expect(mountWrapper.contains(wantToRead)).toEqual(true);
+        expect(mountWrapper.contains(read)).toEqual(true);
+    })
+
 })
-
-
